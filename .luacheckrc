630d009e7098c63da59e424dba6ec685ce552ca4
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-11-18 19:46:04 +0100

    init

---
+++ .luacheckrc
@@ -0,0 +1,12 @@
+read_globals = {
+	-- Stdlib
+	string = {fields = {"split"}},
+	table = {fields = {"copy", "getn"}},
+
+    "monitoring",
+
+	-- Minetest
+	"vector", "ItemStack",
+	"dump", "minetest"
+
+}
---
+++ init.lua
@@ -0,0 +1,21 @@
+
+local function register_effector_metric(nodename, metricname, prettyname)
+	local nodedef = minetest.registered_nodes[nodename]
+	if nodedef and nodedef.effector and nodedef.effector.action_on then
+		local metric_count = monitoring.counter("basic_machines_" .. metricname .. "_count",
+			"number of " .. prettyname .. " executes")
+		local metric_time = monitoring.counter("basic_machines_" .. metricname .. "_time",
+			"total time of " .. prettyname .. " executes in us")
+
+		nodedef.effector.action_on = metric_count.wrap( metric_time.wraptime(nodedef.effector.action_on) )
+
+	end
+end
+
+register_effector_metric("basic_machines:mover", "mover", "Mover")
+register_effector_metric("basic_machines:keypad", "keypad", "Keypad")
+register_effector_metric("basic_machines:detector", "detector", "Detector")
+register_effector_metric("basic_machines:clockgen", "clockgen", "Clockgen")
+register_effector_metric("basic_machines:distributor", "distributor", "Distributor")
+register_effector_metric("basic_machines:light_off", "light_off", "Light off")
+register_effector_metric("basic_machines:light_on", "light_on", "Light on")
